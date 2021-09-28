5e781fa2ce9c0f298dc205a255cee75052d3456f
YerardinPerlaza <yerardinpm@gmail.com>
2021-09-28 02:06:06 +0000

    fix pep8

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,19 +4,20 @@ import requests
 
 
 def top_ten(subreddit):
-   """Finds and prints the titles of the first 10 host posts"""
-   header = {'User-Agent':  'Chrome/66.0.3359.139 Mobile Safari/537.36'}
-   url = "https://api.reddit.com/r/{}/hot/".format(subreddit)
-   response = requests.get(url, headers=header)
+    """Finds and prints the titles of the first 10 host posts"""
 
-   if response.status_code == 200:
-      hot_list = response.json()["data"]["children"]
+    header = {'User-Agent':  'Chrome/66.0.3359.139 Mobile Safari/537.36'}
+    url = "https://api.reddit.com/r/{}/hot/".format(subreddit)
+    response = requests.get(url, headers=header)
 
-      counter = 0
-      for hot in hot_list:
-         if counter == 10:
-            break
-         print(hot["data"]["title"])
-         counter += 1
-   else:
-      print("None")
+    if response.status_code == 200:
+        hot_list = response.json()["data"]["children"]
+
+        counter = 0
+        for hot in hot_list:
+            if counter == 10:
+                break
+            print(hot["data"]["title"])
+            counter += 1
+    else:
+        print("None")
